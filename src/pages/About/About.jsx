5508003acf5f829e7910fafd801a90467dd85b46
J-za <jzarrouk@gmail.com>
2025-08-20 15:26:45 +0200

    feat(about): implement About page layout and content structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,20 @@
+import Banner from '../../components/Banner/Banner'
+import AboutBanner from '../../assets/about-banner.png'
+import Collapse from '../../components/Collapse/Collapse'
+import aboutContent from '../../datas/about-content.json'
+
 function About() {
-  return <h1>A propos</h1>
+  return (
+    <>
+      <Banner
+        imagesrc={AboutBanner}
+        altText="Photo d'une fôret avec des montagnes enneigées en arrière plan"
+      />
+      {aboutContent.map((item) => (
+        <Collapse key={item.id} title={item.title} text={item.text} />
+      ))}
+    </>
+  )
 }
 
 export default About
