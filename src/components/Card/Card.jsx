e892746a296b2cbd78633dd0e3ad0ce78db80e6c
J-za <jzarrouk@gmail.com>
2025-08-21 09:51:18 +0200

    style(card): wrap housing content in <article> tag for semantic structure

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,12 @@ import './Card.scss'
 
 function Card({ id, title, cover }) {
   return (
-    <Link to={`/accomodation/${id}`} className="card">
-      <img src={cover} alt={title} />
-      <h2>{title}</h2>
-    </Link>
+    <article>
+      <Link to={`/accomodation/${id}`} className="card">
+        <img src={cover} alt={title} />
+        <h2>{title}</h2>
+      </Link>
+    </article>
   )
 }
 
