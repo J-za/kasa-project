373bbfb1b0d45c95aa4ee56a7a61fd9280f71fb8
J-za <jzarrouk@gmail.com>
2025-08-21 09:52:12 +0200

    feat(error-page): add 404 error page with navigation and styling

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router'
+import Error404 from '../../assets/404.png'
+import './error.scss'
+
 function Error() {
-  return <h1>Error</h1>
+  return (
+    <section className="error-content">
+      <img src={Error404} alt="Image erreur 404"></img>
+      <p>Oups ! La page que vous&nbsp;demandez n'existe pas.</p>
+      <Link to="/" className="home-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </section>
+  )
 }
 
 export default Error
