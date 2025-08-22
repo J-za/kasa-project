9059786e1005fab8670893a9f0fdebbe691d9918
J-za <jzarrouk@gmail.com>
2025-08-22 10:49:52 +0200

    perf(audit): apply Lighthouse recommendations to improve performance, SEO, and accessibility

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card({ id, title, cover }) {
   return (
     <article>
       <Link to={`/accomodation/${id}`} className="card">
-        <img src={cover} alt={title} />
+        <img src={cover} alt={`Photo du logement : ${title}`} />
         <h2>{title}</h2>
       </Link>
     </article>
