9ee523d5930b0daf4006d96fc4a981d84a83c9b2
J-za <jzarrouk@gmail.com>
2025-08-27 16:34:32 +0200

    feat(accommodation): add accommodation detail page with dynamic content rendering

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,5 +1,64 @@
+import Slideshow from '../../components/Slideshow/Slideshow'
+import accommodations from '../../datas/accommodations.json'
+import Collapse from '../../components/Collapse/Collapse'
+import './accommodation.scss'
+import { useParams, Navigate } from 'react-router'
+import StarDisplay from '../../components/StarDisplay/StarDisplay'
+
 function Accommodation() {
-  return <h1>Logement</h1>
+  const { id } = useParams()
+  const accommodation = accommodations.find((item) => item.id === id)
+
+  if (!accommodation) return <Navigate to="*" replace />
+
+  return (
+    <>
+      <Slideshow pictures={accommodation.pictures} />
+      <section className="property-content">
+        <section className="property-infos">
+          <h1>{accommodation.title}</h1>
+          <p className="location">{accommodation.location}</p>
+          <div className="tag-content">
+            {accommodation.tags.map((tag, index) => (
+              <span key={index} className="tag">
+                {tag}
+              </span>
+            ))}
+          </div>
+        </section>
+        <section className="host-details">
+          <div className="host-content">
+            <p>{accommodation.host.name.replace(' ', '\n')}</p>
+            <img
+              src={accommodation.host.picture}
+              alt={`Photo de ${accommodation.host.name}`}
+            />
+          </div>
+          <StarDisplay rating={accommodation.rating} />
+        </section>
+      </section>
+      <section className="collapse-accommodation">
+        <Collapse
+          key={`${accommodation.id}-description`}
+          title="Description"
+          className="collapse-accommodation-item"
+        >
+          <p>{accommodation.description}</p>
+        </Collapse>
+        <Collapse
+          key={`${accommodation.id}-equipements`}
+          title="Équipements"
+          className="collapse-accommodation-item"
+        >
+          <ul>
+            {accommodation.equipments.map((equipement, index) => (
+              <li key={index}>{equipement}</li>
+            ))}
+          </ul>
+        </Collapse>
+      </section>
+    </>
+  )
 }
 
 export default Accommodation
