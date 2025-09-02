ce984e264e9d7aca194dc17ac00cedf6e2e580f9
J-za <jzarrouk@gmail.com>
2025-09-02 08:14:09 +0200

    feat(banner): apply conditional image filter based on title presence

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,11 @@ import './banner.scss'
 function Banner({ imagesrc, altText, text }) {
   return (
     <section className="banner">
-      <img src={imagesrc} alt={altText} />
+      <img
+        src={imagesrc}
+        alt={altText}
+        className={text ? 'banner-img text' : 'banner-img'}
+      />
       {text && <p>{text}</p>}
     </section>
   )
