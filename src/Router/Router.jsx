5db07b660ea69d61c45654bbdd8aa4bdf41cdcce
J-za <jzarrouk@gmail.com>
2025-09-02 08:46:09 +0200

    feat(router): add ScrollToTop component to reset scroll position on route change

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -5,17 +5,20 @@ import About from '../pages/About/About'
 import Accommodation from '../pages/Accommodation/Accommodation'
 import Error from '../pages/Error/Error'
 import Layout from '../components/Layout/Layout'
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop'
 
 function Router() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/accomodation/:id" element={<Accommodation />} />
-        <Route path="*" element={<Error />} />
-      </Route>
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/accomodation/:id" element={<Accommodation />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </ScrollToTop>
   )
 }
 
